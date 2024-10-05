444954edb88b8c10de8bba1775d78ca959539198
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-10-05 19:54:28 +0900

    Update tkinter_practice.py

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -5,8 +5,8 @@ root = tk.Tk() # ウインドウ作成
 root.title(u"tkinter")
 root.geometry("400x300") # xの前後にはスペースを入れないこと！
 
-#　ラベル
-Static1 = tk.Label(text=u'practice')
+# ラベル
+Static1 = tk.Label(text=u'practice', font = ('MS Gothic', 20), foreground = '#FF00FF', background = 'cyan')
 Static1.pack()
 
 root.mainloop() # ウインドウ表示
